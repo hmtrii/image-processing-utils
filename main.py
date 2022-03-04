0688e416f9675f75a10874fbe3b464f194e2133c
trihuynh <hminhtri19@gmail.com>
2022-03-05 00:08:30 +0700

    add plot_boxes

--- main.py
+++ main.py
@@ -44,18 +44,34 @@ def show_images(images, n_rows, n_columns, fig_size=None, titles=[], title_sizes
     if save_file:
         plt.savefig(save_file)
 
-def plot_boxes(images, boxes):
-    return
+def plot_boxes(image, boxes, color=(255, 0, 0), thickness=2):
+    """ Plot boxes into the image
+
+    Args:
+        image (numpy array): the drawing image
+        boxes (list): drawing boxes. Each box is a list containing [xmin, ymin, xmax, ymax]
+    """
+    
+    if np.array(boxes).ndim == 1:
+        boxes = [boxes]
+    if isinstance(thickness, int):
+        thickness = [thickness] * len(boxes)
+    else:
+        assert len(boxes) == len(thickness), print(f'Assert length of boxes and thickness is the same, len(boxes) = {len(boxes)} and len(color) = {len(thickness)}')
+    if np.array(color).ndim == 1:
+        color = [color] * len(boxes)
+    else:
+        assert len(boxes) == len(color), print(f'Assert length of boxes and color is the same, len(boxes) = {len(boxes)} and len(color) = {len(color)}')
+
+    for box, c, t in zip(boxes, color, thickness):
+        cv2.rectangle(image, (box[0], box[1]), (box[2], box[2]), color=c, thickness=t)
 
 
 if __name__ == '__main__':
-    # image1 = cv2.imread('test-images/GiBehold.png')
-    # image1 = Image.open('test-images/GiBehold.png')
-    # image1 = np.array(image1)
-    # image2 = cv2.imread('test-images/GiPoliceOfficerHead.png')
-    # image3 = cv2.imread('test-images/GiWaterTank.png')
-    # image4 = cv2.imread('test-images/ImInfinite.png')
-    # show_images([image1, image2], 1, 2, \
-    #     save_file='test-output/test.png', sup_title='aaa',\
-    #     titles=['q', 'w'], fig_size=(20, 20), title_sizes=50)
+    # image = cv2.imread('./test-images/puppy.jpg')
+    # boxes = [[100, 120, 600, 500], [1000, 1000, 1200, 1300], [200, 300, 700, 1000]]
+    # colors = [(255, 0, 0), [0, 255, 0], [0, 0, 255]]
+    # thickness = [20, 2, 3]
+    # plot_boxes(image, boxes, color=colors, thickness=thickness)
+    # show_images([image], 1, 1, save_file='test-output/test.png')
     print()

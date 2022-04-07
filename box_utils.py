3c8e6871825f4c21f67b11d55e02e033d9eaa49f
trihuynh <hminhtri19@gmail.com>
2022-04-07 12:05:40 +0700

    add get overlap or not functions

--- box_utils.py
+++ box_utils.py
@@ -23,7 +23,7 @@ def IoU(box1, box2):
     """ Calculate IoU of 2 input boxes
 
     Args:
-            box1, box2 (numpy.array): [xmin, ymin, xmax, ymax]
+        box1, box2 (numpy.array): [xmin, ymin, xmax, ymax]
     Return:
         IoU of box1 and box2
     """
@@ -78,3 +78,109 @@ def nms(boxes, score=[], overlap_thresh=0.3):
         idxs = np.delete(idxs, np.concatenate(([last], \
             np.where(overlap > overlap_thresh)[0])))
     return boxes[pick_id].astype('int')
+
+def get_overlap_boxes(boxes_A, boxes_B, thresh=0.0):
+    """Get boxes in boxes_A that overlap boxes in boxes_B
+    
+    Args:
+        boxes_1, boxes_2: list of boxes [xmin, ymin, xmax, ymax].
+    Return:
+        A list of boxes in boxes_A that overlap boxes in boxes_B
+    """
+    if not isinstance(boxes_A, np.ndarray):
+        boxes_A = np.array(boxes_A).astype(float)
+        if boxes_A.ndim == 1:
+            boxes_A = np.array([boxes_A]).astype(float)
+    if not isinstance(boxes_B, np.ndarray):
+        boxes_B = np.array(boxes_B).astype(float)
+        if boxes_B.ndim == 1:
+            boxes_B = np.array(boxes_B).astype(float)
+
+    xmin_A = boxes_A[:, 0]
+    ymin_A = boxes_A[:, 1]
+    xmax_A = boxes_A[:, 2]
+    ymax_A = boxes_A[:, 3]
+    area_A = (xmax_A - xmin_A + 1) * (ymax_A - ymin_A + 1)
+    idxs = np.arange(len(boxes_A))
+
+    xmin_B = boxes_B[:, 0]
+    ymin_B = boxes_B[:, 1]
+    xmax_B = boxes_B[:, 2]
+    ymax_B = boxes_B[:, 3]
+    area_B = (xmax_B - xmin_B + 1) * (ymax_B - ymin_B + 1)
+
+    pick_idxs = []
+    while len(idxs) > 0:
+        last = len(idxs) - 1
+        i = idxs[last]
+
+        xx1 = np.maximum(xmin_A[i], xmin_B)
+        yy1 = np.maximum(ymin_A[i], ymin_B)
+        xx2 = np.minimum(xmax_A[i], xmax_B)
+        yy2 = np.minimum(ymax_A[i], ymax_B)
+
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+        
+        intersect = w * h
+        union = area_A[i] + area_B - intersect
+        iou = intersect / union
+        idx_overlap = np.where(iou > thresh)[0]
+        if len(idx_overlap) > 0:
+            pick_idxs.append(i)
+        idxs = idxs[:-1]
+
+    return boxes_A[pick_idxs].astype("int")
+
+def get_not_overlap_boxes(boxes_A, boxes_B, thresh=0.0):
+    """Get boxes in boxes_A that are not overlap boxes in boxes_B
+    
+    Args:
+        boxes_1, boxes_2: list of boxes [xmin, ymin, xmax, ymax].
+    Return:
+        A list of boxes in boxes_A that are not overlap boxes in boxes_B
+    """
+    if not isinstance(boxes_A, np.ndarray):
+        boxes_A = np.array(boxes_A).astype(float)
+        if boxes_A.ndim == 1:
+            boxes_A = np.array([boxes_A]).astype(float)
+    if not isinstance(boxes_B, np.ndarray):
+        boxes_B = np.array(boxes_B).astype(float)
+        if boxes_B.ndim == 1:
+            boxes_B = np.array(boxes_B).astype(float)
+
+    xmin_A = boxes_A[:, 0]
+    ymin_A = boxes_A[:, 1]
+    xmax_A = boxes_A[:, 2]
+    ymax_A = boxes_A[:, 3]
+    area_A = (xmax_A - xmin_A + 1) * (ymax_A - ymin_A + 1)
+    idxs = np.arange(len(boxes_A))
+
+    xmin_B = boxes_B[:, 0]
+    ymin_B = boxes_B[:, 1]
+    xmax_B = boxes_B[:, 2]
+    ymax_B = boxes_B[:, 3]
+    area_B = (xmax_B - xmin_B + 1) * (ymax_B - ymin_B + 1)
+
+    pick_idxs = []
+    while len(idxs) > 0:
+        last = len(idxs) - 1
+        i = idxs[last]
+
+        xx1 = np.maximum(xmin_A[i], xmin_B)
+        yy1 = np.maximum(ymin_A[i], ymin_B)
+        xx2 = np.minimum(xmax_A[i], xmax_B)
+        yy2 = np.minimum(ymax_A[i], ymax_B)
+
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+        
+        intersect = w * h
+        union = area_A[i] + area_B - intersect
+        iou = intersect / union
+        idx_overlap = np.where(iou > thresh)[0]
+        if len(idx_overlap) == 0:
+            pick_idxs.append(i)
+        idxs = idxs[:-1]
+
+    return boxes_A[pick_idxs].astype("int")
